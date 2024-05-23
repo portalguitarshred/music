7d5ee07954eaba9787c2bfd0f0368a5cae1757b1
portalguitarshred <portalguitarshred@gmail.com>
2024-05-23 04:54:23 -0300

    Create login.php
    Login

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+include 'db.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM users WHERE email='$email'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $user = $result->fetch_assoc();
+        if (password_verify($password, $user['password'])) {
+            echo "Login realizado com sucesso";
+        } else {
+            echo "Senha incorreta";
+        }
+    } else {
+        echo "Usuário não encontrado";
+    }
+}
+?>
