e4ca86a8692f4cb93dbb4bbc02edc9de0daec402
portalguitarshred <portalguitarshred@gmail.com>
2024-05-23 02:58:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const stations = [
         { name: 'Rock Station', url: 'https://stream.zeno.fm/qupiusi3w5puv' },
-        { name: 'Classic Rocks', url: 'https://stream.zeno.fm/amepggt3jxptv' },
-        
     ];
 
     stations.forEach(station => {
