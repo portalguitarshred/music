dc06c41a50c3d2f66b21c8eaede24852ab9d8496
portalguitarshred <portalguitarshred@gmail.com>
2024-05-26 05:31:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -86,42 +86,47 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     const clockIcon = document.getElementById('clock-icon');
-    const timerModal = document.getElementById('timerModal');
-    const closeModal = document.getElementById('closeModal');
-    const setTimerButton = document.getElementById('setTimer');
-    const timerInput = document.getElementById('timer');
-    
-    clockIcon.addEventListener('click', () => {
-        timerModal.style.display = 'block';
-    });
-    
-    closeModal.addEventListener('click', () => {
+const timerModal = document.getElementById('timerModal');
+const closeModal = document.getElementById('closeModal');
+const setTimerButton = document.getElementById('setTimer');
+const timerInput = document.getElementById('timer');
+
+clockIcon.addEventListener('click', () => {
+    timerModal.style.display = 'block';
+});
+
+closeModal.addEventListener('click', () => {
+    timerModal.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+    if (event.target === timerModal) {
         timerModal.style.display = 'none';
-    });
-    
-    window.addEventListener('click', (event) => {
-        if (event.target === timerModal) {
-            timerModal.style.display = 'none';
-        }
-    });
-    
-    setTimerButton.addEventListener('click', () => {
-        const minutes = parseInt(timerInput.value, 10);
-        if (isNaN(minutes) || minutes <= 0) {
-            alert('Por favor, insira um valor válido de minutos.');
-            return;
-        }
-    
-        const milliseconds = minutes * 60 * 1000;
-        setTimeout(() => {
-            audioPlayer.pause();
-            audioPlayer.currentTime = 0;
-            alert('O temporizador desligou a rádio.');
-        }, milliseconds);
-    
-        timerModal.style.display = 'none';
-        alert(`Temporizador definido para ${minutes} minutos.`);
-    });
+    }
+});
+
+timerModal.addEventListener('click', (event) => {
+    event.stopPropagation();
+});
+
+setTimerButton.addEventListener('click', () => {
+    const minutes = parseInt(timerInput.value, 10);
+    if (isNaN(minutes) || minutes <= 0) {
+        alert('Por favor, insira um valor válido de minutos.');
+        return;
+    }
+
+    const milliseconds = minutes * 60 * 1000;
+    setTimeout(() => {
+        audioPlayer.pause();
+        audioPlayer.currentTime = 0;
+        alert('O temporizador desligou a rádio.');
+    }, milliseconds);
+
+    timerModal.style.display = 'none';
+    alert(`Temporizador definido para ${minutes} minutos.`);
+});
+
 
 
     const shareModal = document.getElementById('shareModal');
