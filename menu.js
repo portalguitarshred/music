1141e9f47637ec64bfd3d2e549eaf6dd0a98ace6
portalguitarshred <portalguitarshred@gmail.com>
2024-05-28 03:17:59 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,38 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-    function openMenu() {
-        console.log("Abrindo menu");
-        const menu = document.querySelector('.menu');
-        menu.classList.add('open');
+    const menu = document.querySelector('.menu');
+
+    function toggleMenu() {
+        menu.classList.toggle('open');
     }
 
-    function closeMenu() {
-        console.log("Fechando menu");
-        const menu = document.querySelector('.menu');
-        menu.classList.remove('open');
-    }
-
-    // Adicionar evento de clique ao ícone do menu
     document.querySelector('.menu-toggle').addEventListener('click', (event) => {
         event.stopPropagation();
-        const menu = document.querySelector('.menu');
-        if (menu.classList.contains('open')) {
-            closeMenu();
-        } else {
-            openMenu();
-        }
+        toggleMenu();
     });
 
-    // Fechar o menu ao clicar fora dele
     document.addEventListener('click', (event) => {
-        const menu = document.querySelector('.menu');
-        const menuToggle = document.querySelector('.menu-toggle');
-        if (!menu.contains(event.target) && !menuToggle.contains(event.target)) {
-            closeMenu();
+        if (!menu.contains(event.target) && !event.target.classList.contains('menu-toggle')) {
+            menu.classList.remove('open');
         }
     });
 
-    // Fechar o menu ao clicar em um link dentro do menu
     document.querySelectorAll('.menu a').forEach(link => {
-        link.addEventListener('click', closeMenu);
+        link.addEventListener('click', () => {
+            menu.classList.remove('open');
+        });
     });
 });
