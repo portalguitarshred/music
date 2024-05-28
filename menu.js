550fcbac62c80c9b98c10485f47c7a315b6f103f
portalguitarshred <portalguitarshred@gmail.com>
2024-05-28 04:53:41 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,19 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const menuToggleCheckbox = document.getElementById('menu-toggle-checkbox');
-    
+    function openMenu() {
+        console.log("Abrindo menu");
+        document.querySelector('.menu').classList.add('open');
+    }
+
+    function closeMenu() {
+        console.log("Fechando menu");
+        document.querySelector('.menu').classList.remove('open');
+    }
+
+    // Adicionar evento de clique ao ícone do menu
+    document.querySelector('.menu-toggle').addEventListener('click', (event) => {
+        event.stopPropagation();
+        openMenu();
+    });
+
     // Fechar o menu ao clicar fora dele
     document.addEventListener('click', (event) => {
         const menu = document.querySelector('.menu');
         const menuToggle = document.querySelector('.menu-toggle');
         if (!menu.contains(event.target) && !menuToggle.contains(event.target)) {
-            menuToggleCheckbox.checked = false;
+            closeMenu();
         }
     });
 
     // Fechar o menu ao clicar em um link dentro do menu
     document.querySelectorAll('.menu a').forEach(link => {
-        link.addEventListener('click', () => {
-            menuToggleCheckbox.checked = false;
-        });
+        link.addEventListener('click', closeMenu);
     });
 });
