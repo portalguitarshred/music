9161ea312cd53896eecb10857c43250e6810d8a4
portalguitarshred <portalguitarshred@gmail.com>
2024-06-06 15:31:26 -0300

    Update playlist-creation.js

--- playlist-creation.js
+++ playlist-creation.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const savePlaylistButton = document.getElementById('save-playlist-button');
     const playlistCoverInput = document.getElementById('playlist-cover-input');
-    const playlistTitleInput = document.getElementById('playlist-title-input'); // Novo campo para o título da playlist
+    const playlistTitleInput = document.getElementById('playlist-title-input'); // Campo para o título da playlist
 
     savePlaylistButton.addEventListener('click', () => {
         const playlistCoverFile = playlistCoverInput.files[0];
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         if (playlistTitle) {
             sessionStorage.setItem('playlistTitle', playlistTitle); // Salvar o título da playlist
+            console.log("Título da playlist salvo:", playlistTitle); // Verificar se o título está sendo salvo
         } else {
             console.log("Nenhum título de playlist inserido.");
         }
@@ -17,8 +18,8 @@ document.addEventListener('DOMContentLoaded', () => {
             const reader = new FileReader();
             reader.onload = function(event) {
                 const coverUrl = event.target.result;
-                console.log("Capa da playlist URL:", coverUrl);
-                sessionStorage.setItem('playlistCover', coverUrl); // Usar sessionStorage para garantir que o dado não se perca
+                console.log("Capa da playlist URL:", coverUrl); // Verificar a URL da capa
+                sessionStorage.setItem('playlistCover', coverUrl); // Salvar a URL da capa
                 window.location.href = 'user-playlist.html';
             };
             reader.readAsDataURL(playlistCoverFile);
