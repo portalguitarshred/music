022dea2ff813ec1e9a729cb889ee4e2e59cb0b93
portalguitarshred <portalguitarshred@gmail.com>
2024-06-06 15:54:19 -0300

    Update playlist-creation.js

--- playlist-creation.js
+++ playlist-creation.js
@@ -1,16 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
     const savePlaylistButton = document.getElementById('save-playlist-button');
-    const playlistTitleInput = document.getElementById('playlist-title-input'); // Campo para o título da playlist
+    const playlistCoverInput = document.getElementById('playlist-cover-input');
+    const playlistNameInput = document.getElementById('playlist-name-input'); // Novo campo de input para o nome da playlist
 
     savePlaylistButton.addEventListener('click', () => {
-        const playlistTitle = playlistTitleInput.value; // Obter o título da playlist
-
-        if (playlistTitle) {
-            sessionStorage.setItem('playlistTitle', playlistTitle); // Salvar o título da playlist
-            console.log("Título da playlist salvo:", playlistTitle); // Verificar se o título está sendo salvo
-            window.location.href = 'user-playlist.html';
+        const playlistCoverFile = playlistCoverInput.files[0];
+        const playlistName = playlistNameInput.value; // Obtendo o valor do nome da playlist
+        if (playlistCoverFile) {
+            const reader = new FileReader();
+            reader.onload = function(event) {
+                const coverUrl = event.target.result;
+                console.log("Capa da playlist URL:", coverUrl);
+                sessionStorage.setItem('playlistCover', coverUrl); // Usar sessionStorage para garantir que o dado não se perca
+                sessionStorage.setItem('playlistName', playlistName); // Salvando o nome da playlist
+                window.location.href = 'user-playlist.html';
+            };
+            reader.readAsDataURL(playlistCoverFile);
         } else {
-            console.log("Nenhum título de playlist inserido.");
+            console.log("Nenhuma capa selecionada.");
+            sessionStorage.removeItem('playlistCover'); // Remover qualquer capa anterior
+            sessionStorage.setItem('playlistName', playlistName); // Salvando o nome da playlist
+            window.location.href = 'user-playlist.html';
         }
     });
 });
