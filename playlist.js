97e197e8f60825f59032fc49aa2f382476cdeb6e
portalguitarshred <portalguitarshred@gmail.com>
2024-06-06 12:35:52 -0300

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -6,15 +6,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const playlistCoverInput = document.getElementById('playlist-cover-input');
 
     createPlaylistButton.addEventListener('click', () => {
+        console.log("Abrindo modal de criação de playlist.");
         createPlaylistModal.style.display = 'block';
     });
 
     closeCreatePlaylistModal.addEventListener('click', () => {
+        console.log("Fechando modal de criação de playlist.");
         createPlaylistModal.style.display = 'none';
     });
 
     window.addEventListener('click', (event) => {
         if (event.target === createPlaylistModal) {
+            console.log("Fechando modal ao clicar fora dele.");
             createPlaylistModal.style.display = 'none';
         }
     });
@@ -22,17 +25,18 @@ document.addEventListener('DOMContentLoaded', () => {
     savePlaylistButton.addEventListener('click', () => {
         const playlistCoverFile = playlistCoverInput.files[0];
         if (playlistCoverFile) {
+            console.log("Capa da playlist selecionada:", playlistCoverFile.name);
             const reader = new FileReader();
             reader.onload = function(event) {
                 const coverUrl = event.target.result;
-                console.log("Capa da playlist URL:", coverUrl); // Log para verificar a URL
+                console.log("Capa da playlist URL:", coverUrl);
                 localStorage.setItem('playlistCover', coverUrl);
                 window.location.href = 'user-playlist.html';
             };
             reader.readAsDataURL(playlistCoverFile);
         } else {
             console.log("Nenhuma capa selecionada. Redirecionando sem capa.");
-            localStorage.removeItem('playlistCover'); // Remover qualquer capa anterior
+            localStorage.removeItem('playlistCover');
             window.location.href = 'user-playlist.html';
         }
     });
@@ -41,8 +45,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const playlistCoverImg = document.getElementById('playlist-cover');
     if (playlistCoverImg) {
         const coverUrl = localStorage.getItem('playlistCover');
-        console.log("Capa da playlist carregada:", coverUrl); // Log para verificar o carregamento da URL
+        console.log("Tentando carregar a capa da playlist:", coverUrl);
         if (coverUrl) {
+            console.log("Capa encontrada. Atualizando o src da imagem.");
             playlistCoverImg.src = coverUrl;
         } else {
             console.log("Nenhuma capa de playlist encontrada no localStorage.");
