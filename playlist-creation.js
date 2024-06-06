1a4b0f00484e7540048ab55cf9e4f95082fc0061
portalguitarshred <portalguitarshred@gmail.com>
2024-06-06 15:24:26 -0300

    Update playlist-creation.js

--- playlist-creation.js
+++ playlist-creation.js
@@ -1,9 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     const savePlaylistButton = document.getElementById('save-playlist-button');
     const playlistCoverInput = document.getElementById('playlist-cover-input');
+    const playlistTitleInput = document.getElementById('playlist-title-input'); // Novo campo para o título da playlist
 
     savePlaylistButton.addEventListener('click', () => {
         const playlistCoverFile = playlistCoverInput.files[0];
+        const playlistTitle = playlistTitleInput.value; // Obter o título da playlist
+
+        if (playlistTitle) {
+            sessionStorage.setItem('playlistTitle', playlistTitle); // Salvar o título da playlist
+        } else {
+            console.log("Nenhum título de playlist inserido.");
+        }
+
         if (playlistCoverFile) {
             const reader = new FileReader();
             reader.onload = function(event) {
