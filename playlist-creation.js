e8e20ed5e3da84bcc69f94c8343f14e0a6c62180
portalguitarshred <portalguitarshred@gmail.com>
2024-06-06 15:09:33 -0300

    Update playlist-creation.js

--- playlist-creation.js
+++ playlist-creation.js
@@ -1,30 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
     const savePlaylistButton = document.getElementById('save-playlist-button');
     const playlistCoverInput = document.getElementById('playlist-cover-input');
-    const playlistTitleInput = document.getElementById('playlist-title-input');
 
     savePlaylistButton.addEventListener('click', () => {
         const playlistCoverFile = playlistCoverInput.files[0];
-        const playlistTitle = playlistTitleInput.value;
-
-        if (playlistTitle) {
-            sessionStorage.setItem('playlistTitle', playlistTitle);
-        } else {
-            console.log("Nenhum título de playlist inserido.");
-        }
-
         if (playlistCoverFile) {
             const reader = new FileReader();
             reader.onload = function(event) {
                 const coverUrl = event.target.result;
                 console.log("Capa da playlist URL:", coverUrl);
-                sessionStorage.setItem('playlistCover', coverUrl);
+                sessionStorage.setItem('playlistCover', coverUrl); // Usar sessionStorage para garantir que o dado não se perca
                 window.location.href = 'user-playlist.html';
             };
             reader.readAsDataURL(playlistCoverFile);
         } else {
             console.log("Nenhuma capa selecionada.");
-            sessionStorage.removeItem('playlistCover');
+            sessionStorage.removeItem('playlistCover'); // Remover qualquer capa anterior
             window.location.href = 'user-playlist.html';
         }
     });
