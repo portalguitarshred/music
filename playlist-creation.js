7cfcf8e08ab8c066c7cb8d11a17e3048fe7859eb
portalguitarshred <portalguitarshred@gmail.com>
2024-06-07 02:27:09 -0300

    Update playlist-creation.js

--- playlist-creation.js
+++ playlist-creation.js
@@ -1,25 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
     const savePlaylistButton = document.getElementById('save-playlist-button');
     const playlistCoverInput = document.getElementById('playlist-cover-input');
-    const playlistNameInput = document.getElementById('playlist-name-input'); // Novo campo de input para o nome da playlist
+    const playlistTitleInput = document.getElementById('playlist-title-input'); // Novo campo de título
 
     savePlaylistButton.addEventListener('click', () => {
+        const playlistTitle = playlistTitleInput.value; // Obter o título da playlist
         const playlistCoverFile = playlistCoverInput.files[0];
-        const playlistName = playlistNameInput.value; // Obtendo o valor do nome da playlist
+
+        // Salvar o título da playlist no sessionStorage
+        sessionStorage.setItem('playlistTitle', playlistTitle);
+
         if (playlistCoverFile) {
             const reader = new FileReader();
             reader.onload = function(event) {
                 const coverUrl = event.target.result;
                 console.log("Capa da playlist URL:", coverUrl);
                 sessionStorage.setItem('playlistCover', coverUrl); // Usar sessionStorage para garantir que o dado não se perca
-                sessionStorage.setItem('playlistName', playlistName); // Salvando o nome da playlist
                 window.location.href = 'user-playlist.html';
             };
             reader.readAsDataURL(playlistCoverFile);
         } else {
             console.log("Nenhuma capa selecionada.");
             sessionStorage.removeItem('playlistCover'); // Remover qualquer capa anterior
-            sessionStorage.setItem('playlistName', playlistName); // Salvando o nome da playlist
             window.location.href = 'user-playlist.html';
         }
     });
