ee589197cc4bd08daebc834b7b055e0ec5ba0417
portalguitarshred <portalguitarshred@gmail.com>
2024-06-07 18:48:24 -0300

    Update playlist-creation.js

--- playlist-creation.js
+++ playlist-creation.js
@@ -9,33 +9,27 @@ document.addEventListener('DOMContentLoaded', () => {
         const songURLs = [];
         const songNames = [];
 
-        // Função para salvar músicas da playlist
-        function savePlaylistSongs() {
-            if (files.length > 0) {
-                let filesProcessed = 0;
-                Array.from(files).forEach((file, index) => {
-                    const reader = new FileReader();
-                    reader.onload = function(event) {
-                        songURLs.push(event.target.result);
-                        songNames.push(file.name);
-                        filesProcessed++;
-                        if (filesProcessed === files.length) {
-                            localStorage.setItem('playlistSongs', JSON.stringify(songURLs));
-                            localStorage.setItem('playlistSongNames', JSON.stringify(songNames));
-                            window.location.href = 'user-playlist.html';
-                        }
-                    };
-                    reader.readAsDataURL(file);
-                });
-            } else {
-                console.log("Nenhuma música selecionada.");
-                localStorage.removeItem('playlistSongs');
-                localStorage.removeItem('playlistSongNames');
-                window.location.href = 'user-playlist.html';
-            }
+        if (files.length > 0) {
+            let filesProcessed = 0; // Contador para verificar quando todos os arquivos foram processados
+            Array.from(files).forEach((file, index) => {
+                const reader = new FileReader();
+                reader.onload = function(event) {
+                    songURLs.push(event.target.result);
+                    songNames.push(file.name);
+                    filesProcessed++;
+                    if (filesProcessed === files.length) {
+                        localStorage.setItem('playlistSongs', JSON.stringify(songURLs));
+                        localStorage.setItem('playlistSongNames', JSON.stringify(songNames));
+                        window.location.href = 'user-playlist.html';
+                    }
+                };
+                reader.readAsDataURL(file);
+            });
+        } else {
+            console.log("Nenhuma música selecionada.");
+            localStorage.removeItem('playlistSongs');
+            localStorage.removeItem('playlistSongNames');
+            window.location.href = 'user-playlist.html';
         }
-
-        // Chamar a função para salvar músicas
-        savePlaylistSongs();
     });
 });
