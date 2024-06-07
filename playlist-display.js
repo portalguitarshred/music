e5f5fb4c7ca376771717e98d89cc305dc218359f
portalguitarshred <portalguitarshred@gmail.com>
2024-06-07 13:29:54 -0300

    Update playlist-display.js

--- playlist-display.js
+++ playlist-display.js
@@ -6,24 +6,16 @@ document.addEventListener('DOMContentLoaded', () => {
     // Exibir a capa da playlist
     if (playlistCoverImg) {
         const coverUrl = sessionStorage.getItem('playlistCover');
-        console.log("Tentando carregar a capa da playlist:", coverUrl);
         if (coverUrl) {
-            console.log("Capa encontrada. Atualizando o src da imagem.");
             playlistCoverImg.src = coverUrl;
-        } else {
-            console.log("Nenhuma capa de playlist encontrada no sessionStorage.");
         }
     }
 
     // Exibir o título da playlist
     if (playlistTitleElem) {
         const playlistName = sessionStorage.getItem('playlistName');
-        console.log("Tentando carregar o nome da playlist:", playlistName);
         if (playlistName) {
-            console.log("Nome da playlist encontrado. Atualizando o título.");
             playlistTitleElem.textContent = playlistName;
-        } else {
-            console.log("Nenhum nome de playlist encontrado no sessionStorage.");
         }
     }
 
