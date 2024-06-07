2dab9526612ef28ef43cb48f524fb2bfdb33d8f6
portalguitarshred <portalguitarshred@gmail.com>
2024-06-07 18:41:49 -0300

    Update playlist-display.js

--- playlist-display.js
+++ playlist-display.js
@@ -6,22 +6,16 @@ document.addEventListener('DOMContentLoaded', () => {
     // Exibir a capa da playlist
     if (playlistCoverImg) {
         const coverUrl = localStorage.getItem('playlistCover');
-        console.log('Carregando a capa da playlist:', coverUrl);
         if (coverUrl) {
             playlistCoverImg.src = coverUrl;
-        } else {
-            console.log('Nenhuma capa de playlist encontrada.');
         }
     }
 
     // Exibir o título da playlist
     if (playlistTitleElem) {
         const playlistName = localStorage.getItem('playlistName');
-        console.log('Carregando o nome da playlist:', playlistName);
         if (playlistName) {
             playlistTitleElem.textContent = playlistName;
-        } else {
-            console.log('Nenhum nome de playlist encontrado.');
         }
     }
 
@@ -30,25 +24,23 @@ document.addEventListener('DOMContentLoaded', () => {
         const songURLs = JSON.parse(localStorage.getItem('playlistSongs')) || [];
         const songNames = JSON.parse(localStorage.getItem('playlistSongNames')) || [];
 
-        if (songURLs.length > 0) {
-            songURLs.forEach((url, index) => {
-                const songDiv = document.createElement('div');
-                songDiv.classList.add('playlist-song');
-                
-                const songInfo = document.createElement('div');
-                songInfo.classList.add('playlist-song-info');
-                songInfo.innerHTML = `<h4>${songNames[index]}</h4>`;
-                
-                const audioElement = document.createElement('audio');
-                audioElement.controls = true;
-                audioElement.src = url;
+        playlistSongsContainer.innerHTML = '';
 
-                songDiv.appendChild(songInfo);
-                songDiv.appendChild(audioElement);
-                playlistSongsContainer.appendChild(songDiv);
-            });
-        } else {
-            console.log('Nenhuma música encontrada.');
-        }
+        songURLs.forEach((url, index) => {
+            const songElem = document.createElement('div');
+            songElem.classList.add('playlist-song');
+            songElem.innerHTML = `
+                <div class="song-details">
+                    <audio controls>
+                        <source src="${url}" type="audio/mpeg">
+                        Your browser does not support the audio element.
+                    </audio>
+                    <div class="playlist-song-info">
+                        <h4>${songNames[index]}</h4>
+                    </div>
+                </div>
+            `;
+            playlistSongsContainer.appendChild(songElem);
+        });
     }
 });
