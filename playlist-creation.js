3f1918443ae13e5d861766d5a37b4bb5fc56dde9
portalguitarshred <portalguitarshred@gmail.com>
2024-06-07 04:54:33 -0300

    Update playlist-creation.js

--- playlist-creation.js
+++ playlist-creation.js
@@ -1,19 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     const savePlaylistButton = document.getElementById('save-playlist-button');
     const playlistCoverInput = document.getElementById('playlist-cover-input');
-    const playlistNameInput = document.getElementById('playlist-name');
+    const playlistNameInput = document.getElementById('playlist-name-input');
     const playlistFilesInput = document.getElementById('playlist-files');
 
     savePlaylistButton.addEventListener('click', () => {
-        const playlistName = playlistNameInput.value;
         const playlistCoverFile = playlistCoverInput.files[0];
+        const playlistName = playlistNameInput.value;
         const files = playlistFilesInput.files;
+        const songURLs = [];
+        const songNames = [];
 
         if (playlistName) {
-            console.log("Nome da playlist salvo:", playlistName);
             sessionStorage.setItem('playlistName', playlistName);
         } else {
-            console.log("Nenhum nome de playlist inserido.");
             sessionStorage.removeItem('playlistName');
         }
 
@@ -21,42 +21,35 @@ document.addEventListener('DOMContentLoaded', () => {
             const reader = new FileReader();
             reader.onload = function(event) {
                 const coverUrl = event.target.result;
-                console.log("Capa da playlist URL:", coverUrl);
                 sessionStorage.setItem('playlistCover', coverUrl);
-                savePlaylistSongs(files); // Salvar músicas depois de salvar a capa
+                saveSongs();
             };
             reader.readAsDataURL(playlistCoverFile);
         } else {
-            console.log("Nenhuma capa selecionada.");
             sessionStorage.removeItem('playlistCover');
-            savePlaylistSongs(files); // Salvar músicas mesmo sem a capa
+            saveSongs();
+        }
+
+        function saveSongs() {
+            if (files.length > 0) {
+                Array.from(files).forEach((file, index) => {
+                    const reader = new FileReader();
+                    reader.onload = function(event) {
+                        songURLs.push(event.target.result);
+                        songNames.push(file.name);
+                        if (index === files.length - 1) {
+                            sessionStorage.setItem('playlistSongs', JSON.stringify(songURLs));
+                            sessionStorage.setItem('playlistSongNames', JSON.stringify(songNames));
+                            window.location.href = 'user-playlist.html';
+                        }
+                    };
+                    reader.readAsDataURL(file);
+                });
+            } else {
+                sessionStorage.removeItem('playlistSongs');
+                sessionStorage.removeItem('playlistSongNames');
+                window.location.href = 'user-playlist.html';
+            }
         }
     });
-
-    function savePlaylistSongs(files) {
-        const songURLs = [];
-        const songNames = [];
-
-        if (files.length > 0) {
-            for (let i = 0; i < files.length; i++) {
-                const file = files[i];
-                const reader = new FileReader();
-                reader.onload = function(event) {
-                    songURLs.push(event.target.result);
-                    songNames.push(file.name);
-                    if (songURLs.length === files.length) {
-                        sessionStorage.setItem('playlistSongs', JSON.stringify(songURLs));
-                        sessionStorage.setItem('playlistSongNames', JSON.stringify(songNames));
-                        window.location.href = 'user-playlist.html';
-                    }
-                };
-                reader.readAsDataURL(file);
-            }
-        } else {
-            console.log("Nenhuma música selecionada.");
-            sessionStorage.removeItem('playlistSongs');
-            sessionStorage.removeItem('playlistSongNames');
-            window.location.href = 'user-playlist.html';
-        }
-    }
 });
