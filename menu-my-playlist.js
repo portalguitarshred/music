7de9f5dbb352ef51230f4de4258f124c20c56501
portalguitarshred <portalguitarshred@gmail.com>
2024-06-10 11:43:42 -0300

    Update menu-my-playlist.js

--- menu-my-playlist.js
+++ menu-my-playlist.js
@@ -1,20 +1,20 @@
 // menu-my-playlist.js
 document.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu');
+    const menuToggle = document.querySelector('.menu-toggle');
+
     function toggleMenu() {
-        const menu = document.querySelector('.menu');
         if (menu) {
             menu.classList.toggle('open');
         }
     }
 
     function closeMenu(event) {
-        const menu = document.querySelector('.menu');
-        if (menu && !menu.contains(event.target) && !document.querySelector('.menu-toggle').contains(event.target)) {
+        if (menu && !menu.contains(event.target) && !menuToggle.contains(event.target)) {
             menu.classList.remove('open');
         }
     }
 
-    const menuToggle = document.querySelector('.menu-toggle');
     if (menuToggle) {
         menuToggle.addEventListener('click', (event) => {
             event.stopPropagation();
@@ -24,10 +24,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.addEventListener('click', closeMenu);
 
-    // Fecha o menu ao clicar em um link dentro dele
     document.querySelectorAll('.menu a').forEach(link => {
         link.addEventListener('click', () => {
-            const menu = document.querySelector('.menu');
             if (menu) {
                 menu.classList.remove('open');
             }
