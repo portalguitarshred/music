adde12e17ec87ba9e45e53ad7a591570f3862646
portalguitarshred <portalguitarshred@gmail.com>
2024-06-13 20:33:48 -0300

    Update playlist-creation.js

--- playlist-creation.js
+++ playlist-creation.js
@@ -1,19 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('Document loaded');
     const savePlaylistButton = document.getElementById('save-playlist-button');
     const playlistCoverInput = document.getElementById('playlist-cover-input');
     const playlistNameInput = document.getElementById('playlist-name');
     const playlistFilesInput = document.getElementById('playlist-files');
 
     savePlaylistButton.addEventListener('click', () => {
-        console.log('Save playlist button clicked');
         const playlistName = playlistNameInput.value.trim();
         const playlistCoverFile = playlistCoverInput.files[0];
         const files = playlistFilesInput.files;
 
         if (!playlistName) {
             alert("Por favor, insira o nome da playlist.");
-            console.log('Nome da playlist ausente');
             return;
         }
 
@@ -24,7 +21,6 @@ document.addEventListener('DOMContentLoaded', () => {
             const reader = new FileReader();
             reader.onload = function(event) {
                 const coverUrl = event.target.result;
-                console.log('Playlist cover loaded:', coverUrl);
                 savePlaylist(playlistName, coverUrl, songURLs, songNames);
             };
             reader.readAsDataURL(playlistCoverFile);
@@ -38,24 +34,14 @@ document.addEventListener('DOMContentLoaded', () => {
         const newPlaylist = { name, cover, songs, songNames };
         playlists.push(newPlaylist);
         savePlaylists(playlists);
-        console.log('Playlist salva:', newPlaylist);
         window.location.href = 'user-my-playlist.html';
     }
 
     function getPlaylists() {
-        try {
-            return JSON.parse(localStorage.getItem('playlists')) || [];
-        } catch (error) {
-            console.error('Erro ao carregar playlists do localStorage:', error);
-            return [];
-        }
+        return JSON.parse(localStorage.getItem('playlists')) || [];
     }
 
     function savePlaylists(playlists) {
-        try {
-            localStorage.setItem('playlists', JSON.stringify(playlists));
-        } catch (error) {
-            console.error('Erro ao salvar playlists no localStorage:', error);
-        }
+        localStorage.setItem('playlists', JSON.stringify(playlists));
     }
 });
