bb79c269678d178f13de782fbcb7d391aba3f22d
portalguitarshred <portalguitarshred@gmail.com>
2024-06-22 02:43:31 -0300

    Update user-my-playlist.js

--- user-my-playlist.js
+++ user-my-playlist.js
@@ -1,15 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const newPlaylistFlag = sessionStorage.getItem('newPlaylist') === 'true';
     const playlists = JSON.parse(sessionStorage.getItem('playlists') || '[]');
-
-    if (newPlaylistFlag) {
-        sessionStorage.setItem('newPlaylist', false);
-    }
 
     playlists.forEach((playlist, index) => {
         const slotId = `playlist-slot-${index + 1}`;
         const playlistSlot = document.getElementById(slotId);
-        
+
         if (playlistSlot) {
             const img = playlistSlot.querySelector('img');
             img.src = playlist.cover || 'capa-playlist.png';
@@ -22,6 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+    // Preencher os slots vazios com a capa padrão
     for (let i = playlists.length; i < 6; i++) {
         const slotId = `playlist-slot-${i + 1}`;
         const playlistSlot = document.getElementById(slotId);
