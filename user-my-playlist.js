3a8490c8db5cbe21b3949d52082a8fce186f2ea2
portalguitarshred <portalguitarshred@gmail.com>
2024-06-25 20:10:56 -0300

    Update user-my-playlist.js

--- user-my-playlist.js
+++ user-my-playlist.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const playlists = JSON.parse(sessionStorage.getItem('playlists') || '[]');
 
-    // Iterar sobre os espaços e preencher com as capas das playlists
     playlists.forEach((playlist, index) => {
         const slotId = `playlist-slot-${index + 1}`;
         const playlistSlot = document.getElementById(slotId);
@@ -11,7 +10,6 @@ document.addEventListener('DOMContentLoaded', () => {
             img.src = playlist.cover || 'capa-playlist.png';
             img.alt = playlist.name;
 
-            // Adicionar evento de clique para abrir a playlist
             playlistSlot.onclick = () => {
                 sessionStorage.setItem('currentPlaylistIndex', index);
                 window.location.href = 'user-playlist.html';
@@ -19,7 +17,6 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    // Preencher os slots vazios com a capa padrão
     for (let i = playlists.length; i < 6; i++) {
         const slotId = `playlist-slot-${i + 1}`;
         const playlistSlot = document.getElementById(slotId);
