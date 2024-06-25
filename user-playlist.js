a77564d100bbe9ef6f2bf4a85492227086cc1664
portalguitarshred <portalguitarshred@gmail.com>
2024-06-25 02:26:49 -0300

    Update user-playlist.js

--- user-playlist.js
+++ user-playlist.js
@@ -48,29 +48,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
             // Adicione evento de clique para tocar a música
             songElem.addEventListener('click', () => {
-                console.log(`Tentando reproduzir a música: ${songTitleText}`);
                 // Se uma música já estiver tocando, pause-a antes de tocar a nova música
                 if (currentAudio) {
-                    console.log('Pausando a música atual');
                     currentAudio.pause();
                     currentAudio.currentTime = 0; // Reinicia a música anterior
                 }
 
                 // Criar um novo objeto Audio para a música selecionada
                 currentAudio = new Audio(songUrl);
-
-                currentAudio.addEventListener('canplaythrough', () => {
-                    console.log('O áudio pode ser reproduzido completamente sem interrupções.');
-                    currentAudio.play().catch(error => {
-                        console.error('Erro ao tentar reproduzir o áudio:', error);
-                    });
+                currentAudio.play().catch(error => {
+                    console.error('Erro ao tentar reproduzir o áudio:', error);
                 });
-
-                currentAudio.addEventListener('error', (e) => {
-                    console.error('Erro ao carregar o áudio:', e);
-                });
-
-                currentAudio.load();
             });
 
             // Adicionar os três pontinhos verticais (menu de opções)
