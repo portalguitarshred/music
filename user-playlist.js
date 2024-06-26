a28e0f94453afbde11c68f1ccadd4392f719698e
portalguitarshred <portalguitarshred@gmail.com>
2024-06-26 03:32:54 -0300

    Update user-playlist.js

--- user-playlist.js
+++ user-playlist.js
@@ -95,17 +95,3 @@ document.addEventListener('DOMContentLoaded', () => {
         window.location.href = 'user-my-playlist.html';
     });
 });
-
-function playAudio(url) {
-    console.log("Iniciando reprodução de áudio:", url);
-    if (window.currentAudio) {
-        window.currentAudio.pause();
-        window.currentAudio.currentTime = 0;
-    }
-    window.currentAudio = new Audio(url);
-    window.currentAudio.play().then(() => {
-        console.log("Reproduzindo áudio:", url);
-    }).catch(error => {
-        console.error('Erro ao tentar reproduzir o áudio:', error);
-    });
-}
